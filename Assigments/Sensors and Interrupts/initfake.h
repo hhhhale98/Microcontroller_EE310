b1afccd2a6c5f13443a974ed5ae462fdf50be442
hhhhale98 <hec25abm@gmail.com>
2026-04-21 12:27:53 -0700

    Add files via upload

---
+++ Assigments/Sensors and Interrupts/initfake.h
@@ -0,0 +1,9 @@
+#ifndef INITFAKE_H
+#define INITFAKE_H
+int SECRET_CODE = 0x23;
+int emergency = 0;
+int check = 0x00;
+int oneD = 0x00;
+int twoD = 0x00;
+int fullN = 0x00;
+#endif /* INITFAKE_H */
